cc0123bb59361251fb3d307647bafa5feb5e7086
nahom <nahumtefera@gmail.com>
2018-10-30 23:26:30 -0700

    creaetd components folder, added Projects and Tasks components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// components
+import Projects from './components/projects/projects'
+import Tasks from './components/tasks/tasks'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Projects />
+        <Tasks />
       </div>
     );
   }
---
+++ src/components/tasks/tasks.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Tasks extends Component {
+
+    render(){
+        return(
+            <div className="tasks-container">
+                Tasks 
+            </div>
+        )
+    }
+}
