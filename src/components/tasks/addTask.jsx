874ad437c7d1ecaad6d51c303b0cd976a48a6d37
nahom <nahumtefera@gmail.com>
2018-10-30 23:39:59 -0700

    added AddTask Component, input to create new tasks

---
+++ src/components/tasks/addTask.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class AddTask extends Component {
+
+    render(){
+        return(
+            <div className="add-task-container">
+                <input className="add-task-input" type="text"/>
+                <button className="add-task-btn">Add Task</button>
+            </div>
+        )
+    }
+}
--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+// components
+import AddTask from './addTask';
+import './tasks.css'
 
 export default class Tasks extends Component {
 
     render(){
         return(
             <div className="tasks-container">
+                <AddTask />
+                <br/><br/><br/>
                 Tasks 
             </div>
         )
