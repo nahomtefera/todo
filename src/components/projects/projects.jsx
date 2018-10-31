38b97053ee25998da5d8ae17a32511e1f61945f7
nahom <nahumtefera@gmail.com>
2018-10-31 01:37:46 -0700

    added styles to projects component

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
+import './projects.css'
 
 export default class Projects extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            projects: ['All Projects', 'GAP', 'Home', 'bravoresume', 'jaminthebay']
+        }
+    }
 
     render(){
         return(
             <div className="projects-container">
-                Projects sidebar
+                <h2 className="projects-title">Projects</h2>
+                <br/>
+                <ul className="projects-list">
+                    {
+                        this.state.projects.map((project, index)=>{
+                            return (
+                                <li key={index} className="project-name">{project}</li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         )
     }
