7db8dcc2783dceebfdb1148aa0da74c1c16f75f5
nahom <nahumtefera@gmail.com>
2018-10-31 01:17:12 -0700

    added svg icons to check and remove tasks, added new styles

--- src/components/tasks/addTask.jsx
+++ src/components/tasks/addTask.jsx
@@ -5,7 +5,7 @@ export default class AddTask extends Component {
     render(){
         return(
             <div className="add-task-container">
-                <input className="add-task-input" type="text"/>
+                <input className="add-task-input" type="text" placeholder="task"/>
                 <button className="add-task-btn">Add Task</button>
             </div>
         )
--- src/components/tasks/task.jsx
+++ src/components/tasks/task.jsx
@@ -5,7 +5,13 @@ export default class Task extends Component {
     render(){
         return(
             <div className="task-container">
+                <div className="task-icons">
+                    <svg className="svg-icons check-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"/></svg>
+                </div>
                 <h2 className="task-title">{this.props.task}</h2>
+                <div className="task-icons">
+                    <svg className="svg-icons trash-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
+                </div>
             </div>
         )
     }
