1d201ab2fd34645c1c43e00fe5e06bf0e6e1167f
nahom <nahumtefera@gmail.com>
2018-10-31 03:57:54 -0700

    added left bar to task-container as a priority identifier

--- src/components/tasks/task.jsx
+++ src/components/tasks/task.jsx
@@ -2,13 +2,16 @@ import React, {Component} from 'react';
 // components
 
 export default class Task extends Component {
+
     render(){
+        let priority=this.props.task.priority;
+
         return(
-            <div className="task-container">
+            <div className={priority === null ? "task-container" : `task-container task-${priority}-priority`}>
                 <div className="task-icons">
                     <svg className="svg-icons check-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"/></svg>
                 </div>
-                <h2 className="task-title">{this.props.task}</h2>
+                <h2 className="task-title">{this.props.task.text}</h2>
                 <div className="task-icons">
                     <svg className="svg-icons trash-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
                 </div>
--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -8,7 +8,29 @@ export default class Tasks extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            tasks: ["Lorem ipsum dolor sit amet consectetur adipiscing, elit est molestie inceptos consequat, congue mi fusce nisi tellus.", "Lorem ipsum dolor sit amet consectetur, adipiscing elit etiam mauris, imperdiet fames velit in.", "Lorem ipsum dolor sit amet consectetur adipiscing elit, ultrices orci aliquam tincidunt egestas placerat, eros cum dictum facilisis tristique quam.", "Lorem ipsum dolor sit amet consectetur adipiscing elit ante primis praesent porttitor nascetur, tempus egestas hendrerit ultrices molestie sed quis rhoncus justo curae."]
+            tasks: [
+                {
+                    text: "Lorem ipsum dolor sit amet consectetur adipiscing, elit est molestie inceptos consequat, congue mi fusce nisi tellus.",
+                    priority: "low",
+                    date: ""
+                },
+                {
+                    text: "Lorem ipsum dolor sit amet consectetur adipiscing elit, ultrices orci aliquam tincidunt egestas placerat, eros cum dictum facilisis tristique quam.",
+                    priority: "med",
+                    date: ""
+                },
+                {
+                    text: "Lorem ipsum dolor sit amet consectetur adipiscing elit ante primis praesent porttitor nascetur, tempus egestas hendrerit ultrices molestie sed quis rhoncus justo curae.",
+                    priority: "low",
+                    date: ""
+                },
+                {
+                    text:"Lorem ipsum dolor sit amet consectetur adipiscing elit ante primis praesent porttitor nascetur, tempus egestas hendrerit ultrices molestie sed quis rhoncus justo curae.",
+                    priority: "high",
+                    date: ""
+                },
+            ]
+
         }
     }
 
