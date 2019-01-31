b4c58d865c595f28f5f939277dd586718f32ab73
nendale <nahom_endale@gap.com>
2019-01-31 10:56:55 -0800

    changed font styles on task container, reset task input when task is added

--- src/components/tasks/addTask.jsx
+++ src/components/tasks/addTask.jsx
@@ -69,6 +69,7 @@ export default class AddTask extends Component {
             projectKey: currentProject,
             priority: priority,
         })
+        this.setState({task:''})
     }
 
     render(){
