19c7127476e76530064186150a615dabf415f9a2
nendale <nahumtefera@gmail.com>
2019-05-17 14:15:30 -0700

    changed logo on tasks page, from written logo to favicon

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../../images/logo.png'
+import logo2 from '../../images/favicon.png';
 import './logo.css'
 
 const Logo = props => {
   return (
     <div className='logo-container'>
-      <img src={logo} className={props.lp ? "logo_lp" : "logo"} alt="logo"/>
+      <img src={logo} className={props.lp ? "logo_lp" : "hide-logo"} alt="logo"/>
+      <img src={logo2} className={props.lp ? "hide-logo" : "logo"} alt="logo"/>
     </div>
   )
 }
